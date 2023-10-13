620f2f5602521510cc8d909111d3fe053d4408e1
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-10-13 16:14:44 -0300

    teste commit

--- src/main/java/com/sofiarocketseat/todolist/user/UserModel.java
+++ src/main/java/com/sofiarocketseat/todolist/user/UserModel.java
@@ -1,32 +1,11 @@
 package com.sofiarocketseat.todolist.user;
 
+import lombok.Data;
+
+@Data //para adicionar getters e setters através do Lombok
 public class UserModel {
 
     public String username;
     public String name;
     public String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    
-    public void setPassword(String password) {
-        this.password = password;
-    }    
 }
